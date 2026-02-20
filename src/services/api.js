3428fde17398683caf8d607a5d6502ae46c08568
ceitec itapipoca <culturamakerprogramacaoeia@gmail.com>
2026-02-20 18:09:03 -0300

    Corrigindo URL da API com /api

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
-const API_URL = 'https://itagame-backend.vercel.app';
+
+const API_URL = 'https://itagame-backend.vercel.app/api';
+
 const api = axios.create({ baseURL: API_URL });
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
 export default api;
