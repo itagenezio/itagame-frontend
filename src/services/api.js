398ba753910060dc24ef392e737148f74ae206b2
ceitec itapipoca <culturamakerprogramacaoeia@gmail.com>
2026-02-20 17:33:25 -0300

    Conectando frontend ao backend Vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'https://itagame-backend.vercel.app'
+const API_URL = 'https://itagame-backend.vercel.app';
 const api = axios.create({ baseURL: API_URL });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
